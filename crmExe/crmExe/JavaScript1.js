65235d8fbb814ba3a1ea372dd639c17976a1c994
SaraKatz <sarittzuriel@gmail.com>
2016-11-07 19:01:19 +0200

    first commit

---
+++ crmExe/crmExe/JavaScript1.js
@@ -0,0 +1,24 @@
+﻿function test() {
+    //var b = Xrm.Page.getAttribute("telephone1").getValue();
+    
+    //var a = Xrm.Page.getAttribute("mobilephone").getValue()
+    //var tmp;
+    
+    //tmp = a;
+    //a = b;
+    //b = tmp;
+    // alert(name + id +  dirty);
+  var fetch1=  "<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>" +
+"  <entity name='lead'>" +
+"    <attribute name='telephone1' />" +
+"    <attribute name='leadid' />" +
+"    <order attribute='fullname' descending='false' />" +
+"    <filter type='and'>" +
+"      <condition attribute='telephone1' operator='not-null' />" +
+"    </filter>" +
+"  </entity>" +
+"</fetch>"
+
+  var entities = XrmServiceToolkit.Soap.Fetch(fetch1);
+  var entities = entities[0].attributes.telephone1.value();
+}
---
+++ crmExe/plugin/Class1.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Text;
+//using System.ServiceModel.Description;
+//using System.ServiceModel;
+using System.Net;
+
+//using Microsoft.Xrm;
+//using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk;
+//using Microsoft.Xrm.Sdk.Messages;
+//using Microsoft.Xrm.Sdk.Query;
+//using Microsoft.Xrm.Sdk.Client;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+//using Microsoft.Crm.Sdk.Messages;
+using System;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace plugins
+{
+    public class pre_validation : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+
+                if (entity.LogicalName != "contact")
+                {
+                    return;
+                }
+
+
+                entity["new_all_phones"] = entity["telephone1"] + "," + entity["mobilephone"];
+
+                IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+                Entity ent = new Entity("contact");
+                Entity entity = service.Retrieve("account", new Guid("2BEBB5DF-6C38-E611-80C9-000C29251C6C"),
+                    new ColumnSet(new string[] { "primarycontactid" }));
+                EntityReference er = (EntityReference)entity["parentcustomerid"];
+                EntityReference updateER = new EntityReference();
+                er.LogicalName = "account";
+                er.Id = new Guid("2DEBB5DF-6C38-E611-80C9-000C29251C6C");
+                if
+
+
+
+
+            }
+
+        }
+
+    }
+}
+
